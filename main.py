573d14e8427b5a71ab2907e139bbe0b8d0b7d0e0
cmccoy995 <127784269+cmccoy995@users.noreply.github.com>
2025-01-21 17:48:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ def main():
     num_words = countWords(text)
     print(f"{num_words} found in the document")
     chars = countChars(text)
-    # chars.sort(reverse=True, key = sort_on)
-    # print(chars)
+    #Sort dict by values
     sortedChars = sorted(chars.items(), key = lambda x:x[1], reverse=True)
+    #Remove non alphabetical
     sortedChars = filter(lambda x: x[0].isalpha(),  sortedChars)
-    # print(list(sortedChars))
     for c in sortedChars:
         print(f"The '{c[0]}' character was found {c[1]} times")
-# def sort_on(dict):
-#     return dict[1]
+#Get number of words
 def countWords(a):
     words = a.split()
     return len(words)
 def getText(path):
     with open(path) as f:
         return f.read()
+#Count of all chars including non-alphabetical
 def countChars(text):
     chars = {}
     for c in text:
